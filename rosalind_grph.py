6551b21945ac3dbb7c1de14b2b29dd58ce1c94bb
Matteo1988 <wulfgar88@alice.it>
2020-06-20 16:54:28 +0200

    Add files via upload

---
+++ rosalind_grph.py
@@ -0,0 +1,16 @@
+#first save rosalind_grph.txt as prova.txt
+
+from Bio import SeqIO
+
+sequenze={}
+
+k = 3 # number of nucleotides that overlaps
+
+for seq in SeqIO.parse("prova.fasta", "fasta"):
+
+    sequenze[seq.id] = str(seq.seq)
+
+for key1,value1 in sequenze.items():
+    for key2, value2 in sequenze.items():
+        if key1 != key2 and value1[0:k] == value2[-k:]:
+            print(key2, key1)
