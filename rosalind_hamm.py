e7f09791840a8c18f6c6e3c2997421bcadafae20
Matteo1988 <selvadec88@gmail.com>
2020-10-08 15:55:55 +0200

    Add files via upload

--- rosalind_hamm.py
+++ rosalind_hamm.py
@@ -1,42 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-in_file = open ("rosalind_hamm.txt","r")
-
-
-num = 0
-
-result = [line.split() for line in in_file.readlines()]
-
-l1 = str(result[0])
-l2 = str(result[1])
-
-list1 = l1[2:-2]
-list2 = l2[2:-2]
-
-for i in range(len(list1)):
-    if list1[i] != list2[i]:
-        num = num+1
-
-print(num)
-
-#for line in in_file:
+with open("rosalind_hamm.txt","r") as f:
+    seq = f.read()
     
-    
-    
-    
-    
+print(seq)
+l = list(seq.split("\n"))
+print(l)
+s1 = l[0]
+s2 = l[1]
+
+print(s1)
+print(s2)
+
+dist = 0
+
+for i in range(len(s1)):
+    if s1[i] != s2[i]:
+        dist = dist+1
         
-
-    
-    
-    
-
-
-
-
-
+print(dist)
